06af864b70eae86c6227059d28731569c7a8813b
alex20b <alex20b81@gmail.com>
2026-03-05 23:58:12 +0200

    fix

--- lab2.c
+++ lab2.c
@@ -1,20 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int function(int x){
-	x=x-2;
-	x=function(x);
+int function(int x, int* i)
+{
+	if(x != 0 && x > 0)
+	{
+		x -= 2;
+		(*i)++;
+		x = function(x, i);
+	}
+
 	return x;
 }
 
-int main(){
+int main()
+{
 	int a=30, i=0;
-	
-	while (a>0){
-		a=function(a);
-		i++;
-	}
-	printf("%d", i);
+
+	a = function(a, &i);
+
+	printf("%d \n", i);
 	return 0;	
 }
-
